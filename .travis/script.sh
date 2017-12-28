7a14f25c2997fd88f11c0d65812320b9ca990bfa
wiredfool <eric-github@soroos.net>
2017-12-28 10:16:36 +0000

    Coverage path changed for travis runs

--- .travis/script.sh
+++ .travis/script.sh
@@ -6,8 +6,8 @@ coverage erase
 python setup.py clean
 CFLAGS="-coverage" python setup.py build_ext --inplace
 
-coverage run --append --include=PIL/* selftest.py
-coverage run --append --include=PIL/* -m nose -vx Tests/test_*.py
+coverage run --append --include=src/PIL/* selftest.py
+coverage run --append --include=src/PIL/* -m nose -vx Tests/test_*.py
 pushd /tmp/check-manifest && check-manifest --ignore ".coveragerc,.editorconfig,*.yml,*.yaml,tox.ini" && popd
 
 # Docs
