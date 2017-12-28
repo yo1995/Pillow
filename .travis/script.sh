c23c8f0338a315a52967125f332a97b92073c2b4
wiredfool <eric-github@soroos.net>
2017-12-28 11:03:21 +0000

    Don't add /src/PIL to the sys.path

--- .travis/script.sh
+++ .travis/script.sh
@@ -6,8 +6,8 @@ coverage erase
 make clean
 make install-coverage
 
-coverage run --append --include=src/PIL/* selftest.py
-coverage run --append --include=src/PIL/* -m nose -vx Tests/test_*.py
+coverage run --append  selftest.py
+coverage run --append  -m nose -vx Tests/test_*.py
 pushd /tmp/check-manifest && check-manifest --ignore ".coveragerc,.editorconfig,*.yml,*.yaml,tox.ini" && popd
 
 # Docs
